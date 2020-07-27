3fd88603dd62114c59d8ef11417d9010e7a26746
asharib90 <asharibahmed143@gmial.com>
2020-07-27 04:10:23 -0700

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -3,30 +3,41 @@ const handleRegister=(bcrypt,db)=>(req,res)=>{
     if(!email || !name || !password){
         res.status(400).json('Incorrect form submission')
     }else{
-        const hash =bcrypt.hashSync(password)    
-        db.transaction(trx=>{
-            trx.insert({
-                hash:hash,
-                email:email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail=>{
-                return trx('users')
-                .returning('*')
-                .insert({
-                    email:loginEmail[0],
-                    name:name,
-                    joined:new Date()
-                })    
-                .then(user=>{
-                    res.json(user[0])
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)        
-        }) 
-        .catch(err=> res.status(400).json('unable to register'))
+        // const hash =bcrypt.hashSync(password)    
+        // db.transaction(trx=>{
+        //     trx.insert({
+        //         hash:hash,
+        //         email:email
+        //     })
+        //     .into('login')
+        //     .returning('email')
+        //     .then(loginEmail=>{
+        //         return trx('users')
+        //         .returning('*')
+        //         .insert({
+        //             email:loginEmail[0],
+        //             name:name,
+        //             joined:new Date()
+        //         })    
+        //         .then(user=>{
+        //             res.json(user[0])
+        //         })
+        //     })
+        //     .then(trx.commit)
+        //     .catch(trx.rollback)        
+        // }) 
+        // .catch(err=> res.status(400).json('unable to register'))
+        db('users')
+        .returning('*')
+        .insert({
+            email:email,
+            name:name,
+            joined:new Date()
+        })
+        .then(user=>{
+            res.json(user[0])
+        })
+        .catch(err=> res.status(400).json('unable to register'))        
     }
 }
 
