ed1a309fe0731b2b571e2000e3776acb93b16d95
asharib90 <asharibahmed143@gmial.com>
2020-07-27 04:14:37 -0700

    'fix'

--- controllers/register.js
+++ controllers/register.js
@@ -3,41 +3,30 @@ const handleRegister=(bcrypt,db)=>(req,res)=>{
     if(!email || !name || !password){
         res.status(400).json('Incorrect form submission')
     }else{
-        // const hash =bcrypt.hashSync(password)    
-        // db.transaction(trx=>{
-        //     trx.insert({
-        //         hash:hash,
-        //         email:email
-        //     })
-        //     .into('login')
-        //     .returning('email')
-        //     .then(loginEmail=>{
-        //         return trx('users')
-        //         .returning('*')
-        //         .insert({
-        //             email:loginEmail[0],
-        //             name:name,
-        //             joined:new Date()
-        //         })    
-        //         .then(user=>{
-        //             res.json(user[0])
-        //         })
-        //     })
-        //     .then(trx.commit)
-        //     .catch(trx.rollback)        
-        // }) 
-        // .catch(err=> res.status(400).json('unable to register'))
-        db('users')
-        .returning('*')
-        .insert({
-            email:email,
-            name:name,
-            joined:new Date()
-        })
-        .then(user=>{
-            res.json(user[0])
-        })
-        .catch(err=> res.status(400).json('unable to login'))        
+        const hash =bcrypt.hashSync(password)    
+        db.transaction(trx=>{
+            trx.insert({
+                hash:hash,
+                email:email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail=>{
+                return trx('users')
+                .returning('*')
+                .insert({
+                    email:loginEmail[0],
+                    name:name,
+                    joined:new Date()
+                })    
+                .then(user=>{
+                    res.json(user[0])
+                })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)        
+        }) 
+        .catch(err=> res.status(400).json('unable to register'))
     }
 }
 
