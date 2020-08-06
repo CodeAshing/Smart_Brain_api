14f3a8c38ac64bbf839bd05adf569842326be022
Asharib Ahmed <Asharibahmed143@gmail.com>
2020-08-06 17:23:39 +0500

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -1,33 +1,32 @@
-const handleRegister = (req, res, db, bcrypt) => {
+const handleRegister = (req, res, bcrypt,db) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
-  }else{
-        const hash = bcrypt.hashSync(password);
-            db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0],
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0]);
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-            })
-            .catch(err => res.status(400).json('unable to register'))
-    }
+  }
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
