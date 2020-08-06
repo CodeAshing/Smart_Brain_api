c70dbc7d604dd3ffae07ce4ec7f17a5664961d61
Asharib Ahmed <Asharibahmed143@gmail.com>
2020-08-06 17:29:24 +0500

    fix

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const db=knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ssl:{
-        rejectUnauthorized: false
-      }
+      ssl:true,
     }
   });
 
