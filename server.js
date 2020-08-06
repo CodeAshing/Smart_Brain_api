e82558755311bd9f65b3ca6a7e771a425d264ba5
Asharib Ahmed <Asharibahmed143@gmail.com>
2020-08-06 05:05:11 +0500

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { profileHandler } = require('./controllers/profile')
 const db=knex({
     client: 'pg',
     connection: {
-      connectString : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl:{
         rejectUnauthorized: false
       }
